eb6dab727a5ed3c444015cfc799316707d070b71
shaoxingguo <shaoxingguo@Mac-mini.local>
2019-07-17 10:37:20 +0800

    初始化项目

---
+++ WYNews/WYNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYNews
+//
+//  Created by shaoxingguo on 2019/7/17.
+//  Copyright © 2019 shaoxingguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
